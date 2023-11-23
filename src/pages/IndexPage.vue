f61c6b162e2bd4f96b53b17cac741d1146adf64f
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2023-11-23 13:15:32 +0100

    chore: Initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Harum impedit temporibus mollitia, eaque esse libero nulla
+      natus pariatur unde cupiditate iure iusto ullam maxime quibusdam
+      ut cum enim dolores numquam.
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
